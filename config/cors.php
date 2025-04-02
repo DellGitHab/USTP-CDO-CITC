bbb676590dcb24698bae8475cda5b073a1bbdeff
Wendelle <wendell@gmail.comm>
2025-04-02 09:36:19 +0800

    backendPHP

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,10 @@
 <?php
-// Allow access only from your React app
 header("Access-Control-Allow-Origin: *"); 
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
-// Handle preflight OPTIONS requests
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(204); // No content
+    http_response_code(204);
     exit;
 }
 ?>
